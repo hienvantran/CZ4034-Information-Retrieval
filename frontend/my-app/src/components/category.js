82bd0f76783e8d1e843f12db53629e752d31e527
eriCazorla <czheric@hotmail.com>
2022-03-16 22:44:02 +0800

    Fix Category Text

--- frontend/my-app/src/components/category.js
+++ frontend/my-app/src/components/category.js
@@ -6,19 +6,16 @@ const category = () => {
     return (
         <>
 
-            <h1>Type Of Vaccine(s)</h1>
-            <div className="container-fluid mx-2">
-                <div className="row mt-5 mx-2">
-                    <div className="col-md-3">
+            <h1 className="mx-3 my-5">Type Of Vaccine(s)</h1>
+            <div className="container-fluid mx-3">
+                <div className="row mt-5">
+                    <div className="col-md-2">
                         <button className="btn btn-warning w-100 mb-4">Pfizer</button>
                         <button className="btn btn-warning w-100 mb-4">Morderna</button>
                         <button className="btn btn-warning w-100 mb-4">Sinopharm</button>
                         <button className="btn btn-warning w-100 mb-4">Novavax</button>
                         <button className="btn btn-warning w-100 mb-4">All Vaccines</button>
                     </div>
-                    {/* <div className="col-md-9">
-                        
-                        </div> */}
                 </div>
             </div>
 
