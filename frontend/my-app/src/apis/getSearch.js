42ed82e8f8438b6f99340a52b89724a1915ae390
TRAN HIEN VAN <54805500+hienvantran@users.noreply.github.com>
2022-03-29 15:29:00 +0800

    set limit 100 for solr res

--- frontend/my-app/src/apis/getSearch.js
+++ frontend/my-app/src/apis/getSearch.js
@@ -4,13 +4,13 @@ export const getSearch = _.memoize(async params => {
     let term = params.term;
     let sort = params.sort;
     let sorting = '';
-    console.log("this is sort: " + sort);
-    sort === '' ? sorting = '' : sorting = sort + '%20desc';
-    console.log("this is sort: " + sorting);
 
-    const res = await fetch(`http://localhost:8983/solr/CZ4034/spell?debugQuery=true&df=text&indent=true&q.op=AND&q=${term}&sort=${sorting}&wt=json`)
+    sort === '' ? sorting = '' : sorting = sort + '%20desc';
+
+
+    const res = await fetch(`http://localhost:8983/solr/CZ4034/spell?debugQuery=true&df=text&indent=true&q.op=AND&q=${term}&sort=${sorting}&rows=100&wt=json`)
     if (res.status !== 200) return [];
     const tweetsArray = await res.json();
-    console.log(tweetsArray.response.numFound)
+    console.log(tweetsArray.response)
     return tweetsArray;
 });
